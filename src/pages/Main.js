6ad8a3f06a70087bb611e26086210ac4f47a8b0f
JOHNKIM-KK <76652614+JOHNKIM-KK@users.noreply.github.com>
2021-08-17 22:20:26 +0900

    [Feat] 메인페이지에 sort 컴포넌트 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import LiveTime from "components/LiveTime";
+import Sort from "pages/Sort";
 import styled from "styled-components";
 
 function Main() {
@@ -17,7 +18,7 @@ function Main() {
           </Time>
         </Wrapper>
       </ClockContainer>
-
+      <Sort />
       <ClockContainer>
         <Wrapper>
           <City>New York</City>
@@ -32,6 +33,7 @@ function Main() {
 
 const Container = styled.div`
   width: 100%;
+  height: 100vh;
   max-width: 512px;
   background: rgb(243, 243, 243);
 `;
@@ -40,6 +42,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 50px;
   padding: 10px;
   background-color: #fff;
   box-shadow: 0 4px 28px 0 rgb(0 0 0 / 10%);
@@ -60,7 +63,7 @@ const LogoImg = styled.img`
 `;
 
 const ClockContainer = styled.div`
-  padding: 10px 20px 15px 20px;
+  padding: 0px 20px;
 `;
 
 const Wrapper = styled.div`
